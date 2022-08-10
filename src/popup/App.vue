07bb9723f506e7e25a3c38c6bcac89a47f354098
caiwl <>
2022-08-10 15:49:00 +0800

    init

---
+++ src/popup/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    hello chrome extension
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="postcss">
+body,html {
+  @apply w-96 h-96 p-2
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,38 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pages: {
+    popup: {
+      template: 'public/browser-extension.html',
+      entry: './src/popup/main.js',
+      title: 'Popup'
+    },
+    options: {
+      template: 'public/browser-extension.html',
+      entry: './src/options/main.js',
+      title: 'Options'
+    }
+  },
+
+  pluginOptions: {
+    browserExtension: {
+      componentOptions: {
+        background: {
+          entry: 'src/background.js'
+        },
+        contentScripts: {
+          entries: {
+            'content-script': [
+              'src/content-scripts/content-script.js'
+            ]
+          }
+        }
+      }
+    }
+  },
+
+  chainWebpack: config => {
+    config.optimization.delete('splitChunks')
+  },
 })
